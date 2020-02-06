d6b73d7cb2bb4a8dd65fffa9f756566c4272b0e3
Paul <paulchenpmc@gmail.com>
2020-02-06 13:16:08 -0700

    Add calculation functionality

--- calculator.js
+++ calculator.js
@@ -1,20 +1,41 @@
+function checkDisplayState() {
+    display_tag = document.getElementById("display");
+    display_text = display_tag.textContent;
+    switch (display_text) {
+        case "Error":
+            display_tag.textContent = "";
+            break;
+    }
+}
+
 function buttonClick(buttonVal) {
-    console.log("buttonclick");
+    checkDisplayState();
+    display_tag = document.getElementById("display");
+    display_tag.textContent += buttonVal;
 }
 
 function evaluateExpression() {
-    console.log("eval click");
+    checkDisplayState();
+    display_tag = document.getElementById("display");
+    expression = display_tag.textContent;
+    try {
+        result = eval(expression);
+    } catch (e) {
+        if (e instanceof SyntaxError) {
+            display_tag.textContent = "Error";
+        }
+        return;
+    }
+    display_tag.textContent = result;
 }
 
 function clearLastDisplay() {
-    console.log("clear click");
+    checkDisplayState();
+    display_tag = document.getElementById("display");
+    display_tag.textContent = display_tag.textContent.slice(0, -1);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("eval-button").addEventListener("click", evaluateExpression);
     document.getElementById("clear-button").addEventListener("click", clearLastDisplay);
-    let elements = document.getElementsByClassName("default-button");
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("click", buttonClick);
-    }
 });
