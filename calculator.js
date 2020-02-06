b428ef2bc5d37d12d7e0a00dc40780951e489bb8
Paul <paulchenpmc@gmail.com>
2020-02-05 23:02:42 -0700

    JS loads after DOM inits

--- calculator.js
+++ calculator.js
@@ -10,9 +10,11 @@ function clearLastDisplay() {
     console.log("clear click");
 }
 
-document.getElementById("eval-button").addEventListener("click", evaluateExpression);
-document.getElementById("clear-button").addEventListener("click", clearLastDisplay);
-let elements = document.getElementsByClassName("default-button");
-for (let i = 0; i < elements.length; i++) {
-    elements[i].addEventListener("click", buttonClick);
-}
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("eval-button").addEventListener("click", evaluateExpression);
+    document.getElementById("clear-button").addEventListener("click", clearLastDisplay);
+    let elements = document.getElementsByClassName("default-button");
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("click", buttonClick);
+    }
+});
